fb40473a9e4cbdd8e8a3c042d2f8cb4f527cea48
SJT1988 <41922761+SJT1988@users.noreply.github.com>
2025-03-25 21:46:12 -0500

    Appended python file
    This is the whole addon. I may include an example .blend file at some point.

---
+++ RenderResFromCamBG.py
@@ -0,0 +1,29 @@
+bl_info = {
+    "name": "Set Render Resolution from Camera BG Image Dimensions",
+    "author": "Spencer Trumbore",
+    "version": (1,0,0),
+    "blender": (4,4,0),
+    "location": "Scene Properties > Render",
+    "description": "Automatically matches render resolution to camera background image dimensions",
+    "category": "Render",
+}
+
+import bpy
+
+def camera_handler(scene):
+    if not scene.camera or not scene.camera.data:
+        return
+        
+    cam_data = scene.camera.data
+    if cam_data.background_images:
+        bg_img = cam_data.background_images[0].image
+        if bg_img:
+            scene.render.resolution_x = bg_img.size[0]
+            scene.render.resolution_y = bg_img.size[1]
+
+def register():
+    bpy.app.handlers.depsgraph_update_post.append(camera_handler)
+
+def unregister():
+    if camera_handler in bpy.app.handlers.depsgraph_update_post:
+        bpy.app.handlers.depsgraph_update_post.remove(camera_handler)
